7fc9e3a6cd6207f0986b69d86e37611e54bc5cb3
unknown <borgbean@sammich.info>
2024-12-11 00:10:32 -0500

    day 11

---
+++ day11-2.js
@@ -0,0 +1,46 @@
+import readInputToLines from "./util/util.js";
+
+export default function day11() {
+    let input = readInputToLines('inputs/11.txt');
+
+    let initial = input[0].split(' ').map(Number);
+
+    let dp = new Array(75+1).fill(0).map(x => []);
+    let result = 0n;
+
+
+    for(let num of initial) {
+        result += count(75, num);
+    }
+
+    return result;
+
+
+    function count(i, num) {
+        if(i === 0) {
+            return 1n;
+        }
+
+        if(dp[i][num]) { return dp[i][num]; }
+
+        let ret;
+
+        if(num === 0) {
+            ret = count(i-1, 1);
+        } else {
+            let digits = num+'';
+            if((digits.length%2) === 0) {
+                ret = count(i-1, Number(digits.substring(0, digits.length/2))) 
+                        + count(i-1, Number(digits.substring(digits.length/2)));
+            } else {
+                ret = count(i-1, Number(num*2024));
+            }
+        }
+
+        dp[i][num] = ret;
+
+        return ret;
+    }
+}
+
+console.log(day11());
