acc061bf000481573d44de5d18636493baf6f376
Michael Martin <michaelm@Michaels-MacBook-Air.local>
2024-12-25 02:41:30 -0500

    use day 22 part 2 implementation for part 1 (perf boost)

--- day22-1.js
+++ day22-1.js
@@ -1,32 +1,27 @@
 import readInputToLines from "./util/util.js";
 
-
+const MASK = (1<<24)-1
 
 export default function day22() {
     let input = readInputToLines('inputs/22.txt');
 
-    let result = 0n;
+
+    let result = 0;
     for(let secretNumS of input) {
-        let secretNum = BigInt(secretNumS);
+        let secretNum = Number(secretNumS);
 
         for(let i = 0; i < 2000; ++i) {
-            secretNum = prune(mix(secretNum, secretNum*64n));
-            secretNum = prune(mix(secretNum, secretNum / 32n));
-            secretNum = prune(mix(secretNum, secretNum*2048n));
+            secretNum = (secretNum ^ secretNum<<6) & MASK;
+            secretNum = (secretNum ^ secretNum>>>5); //don't technically need to mask here
+            secretNum = (secretNum ^ secretNum<<11) & MASK;
         }
-
         result += secretNum;
     }
+
 
     return result;
 }
 
-function mix(a, b) {
-    return a ^ b;
-}
-function prune(a) {
-    return a % 16777216n;
-}
 
 console.time()
 console.log(day22());
